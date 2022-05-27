de21024a347d4799feb66a03b215781e18956630
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 16:54:51 +0530

    Update alive.py

--- Vaibhav/modules/alive.py
+++ Vaibhav/modules/alive.py
@@ -5,20 +5,20 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from SuzuneHorikita.events import register
-from SuzuneHorikita import telethn as tbot
+from Vaibhav.events import register
+from Vaibhav import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/6001b9d2ba6d3c183b979.mp4"
+PHOTO = "https://telegra.ph/file/e782fdd077edd25a332d6.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm SuzuneHorikita** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm a Group Management Bot** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [𓆩𝐀ᴋ𝐀s𝐇𓆪™](https://t.me/TheVenomXD)** \n\n"
+  TEXT += f"⚪ **My Master : [Vaibhav](https://t.me/x0x_VAIBHAV_x0x)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Suzune_Superbot?start=help"), Button.url("Support", "https://t.me/Suzune_Support")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Dedsec_xbot?start=help"), Button.url("Support", "https://t.me/dedsecsupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
