7b4fac4c38565ce217d919df4010be571014a374
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 16:33:05 +0530

    Update approve_sql.py

--- Vaibhav/modules/sql/approve_sql.py
+++ Vaibhav/modules/sql/approve_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from SuzuneHorikita.modules.sql import BASE, SESSION
+from Vaibhav.modules.sql import BASE, SESSION
 
 class Approvals(BASE):
     __tablename__ = "approval"
