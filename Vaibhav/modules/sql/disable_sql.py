2d7269b16d96c03f2b3cc2f1e621fb84e3690acd
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 16:37:09 +0530

    Update disable_sql.py

--- Vaibhav/modules/sql/disable_sql.py
+++ Vaibhav/modules/sql/disable_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from SuzuneHorikita.modules.sql import BASE, SESSION
+from Vaibhav.modules.sql import BASE, SESSION
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
 
