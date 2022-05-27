9594be7cc326728b950eb720ca296ec6e55e0edd
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 12:12:02 +0530

    Update mongo.py

--- Vaibhav/mongo.py
+++ Vaibhav/mongo.py
@@ -3,13 +3,13 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from SuzuneHorikita import MONGO_DB_URI
-from SuzuneHorikita.confing import get_int_key, get_str_key
+from Vaibhav import MONGO_DB_URI
+from Vaibhav.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "SuzuneHorikita"
+MONGO_DB = "Vaibhav"
 
 
 client = MongoClient()
