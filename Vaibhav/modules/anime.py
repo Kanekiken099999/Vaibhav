24a1f1632e351edf012ef9f866453aee2d93ef05
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 16:57:17 +0530

    Update anime.py

--- Vaibhav/modules/anime.py
+++ Vaibhav/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from SuzuneHorikita import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from SuzuneHorikita.modules.disable import DisableAbleCommandHandler
+from Vaibhav import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from Vaibhav.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler
