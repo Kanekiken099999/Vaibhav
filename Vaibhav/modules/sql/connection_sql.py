d2dcaf1394363203d59b7a772ed57c4280dbf051
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 16:36:02 +0530

    Update connection_sql.py

--- Vaibhav/modules/sql/connection_sql.py
+++ Vaibhav/modules/sql/connection_sql.py
@@ -5,7 +5,7 @@ from typing import Union
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from SuzuneHorikita.modules.sql import SESSION, BASE
+from Vaibhav.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
