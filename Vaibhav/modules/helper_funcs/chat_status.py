23c4a97a686917856ffaea01bdde6998f7a36c54
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 12:26:11 +0530

    Update chat_status.py

--- Vaibhav/modules/helper_funcs/chat_status.py
+++ Vaibhav/modules/helper_funcs/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from SuzuneHorikita import (
+from Vaibhav import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from SuzuneHorikita.modules import connection
+from Vaibhav.modules import connection
 
 connected = connection.connected
