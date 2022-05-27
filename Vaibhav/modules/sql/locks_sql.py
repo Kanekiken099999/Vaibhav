056b5ba197de91915d31c6451b882f99bc8ffc75
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 16:39:28 +0530

    Update locks_sql.py

--- Vaibhav/modules/sql/locks_sql.py
+++ Vaibhav/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from SuzuneHorikita.modules.sql import SESSION, BASE
+from Vaibhav.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
