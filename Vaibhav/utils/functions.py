339c56531457a768c6bb338c8d6b56e196613ca6
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 12:21:35 +0530

    Update functions.py

--- Vaibhav/utils/functions.py
+++ Vaibhav/utils/functions.py
@@ -32,9 +32,9 @@ import speedtest
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from pyrogram.types import Message
 
-from SuzuneHorikita import aiohttpsession as aiosession
-from SuzuneHorikita.ex_plugins.dbfunctions import start_restart_stage
-from SuzuneHorikita.utils.http import get, post
+from Vaibhav import aiohttpsession as aiosession
+from Vaibhav.ex_plugins.dbfunctions import start_restart_stage
+from Vaibhav.utils.http import get, post
 
 
 async def restart(m: Message):
