6c756ca77e0c1673cdfa02a8220258436cb8c0df
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 15:43:46 +0530

    Update __init__.py

--- Vaibhav/modules/sql/__init__.py
+++ Vaibhav/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from SuzuneHorikita import DB_URI
+from Vaibhav import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
