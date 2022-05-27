0068c18d756035e7e66d89095c841dc90dc6e819
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 16:33:27 +0530

    Update blacklist_sql.py

--- Vaibhav/modules/sql/blacklist_sql.py
+++ Vaibhav/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from SuzuneHorikita.modules.sql import SESSION, BASE
+from Vaibhav.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
