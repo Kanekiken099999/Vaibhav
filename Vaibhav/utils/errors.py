45198a724af6a7b5d700f4f2a8afdde1f95b3673
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 12:20:39 +0530

    Update errors.py

--- Vaibhav/utils/errors.py
+++ Vaibhav/utils/errors.py
@@ -7,7 +7,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from SuzuneHorikita import LOGGER, pbot as app
+from Vaibhav import LOGGER, pbot as app
 
 
 def split_limits(text):
