18db964566b95da726d294e767b475619f4759b4
Rin Okumura <101918125+Kanekiken099999@users.noreply.github.com>
2022-05-27 16:41:44 +0530

    Update notes_sql.py

--- Vaibhav/modules/sql/notes_sql.py
+++ Vaibhav/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from SuzuneHorikita.modules.helper_funcs.msg_types import Types
-from SuzuneHorikita.modules.sql import BASE, SESSION
+from Vaibhav.modules.helper_funcs.msg_types import Types
+from Vaibhav.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, String, UnicodeText, distinct, func
 from sqlalchemy.sql.sqltypes import BigInteger
 
